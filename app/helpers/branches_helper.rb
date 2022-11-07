d1b61e8f06e8c43bc524617909e68885ea760ea9
enzoperce35 <enzoperce35@gmail.com>
2022-11-07 09:36:50 +0800

    built nested dropdown menus for market and premade items

--- app/helpers/branches_helper.rb
+++ app/helpers/branches_helper.rb
@@ -1,2 +1,7 @@
 module BranchesHelper
+  
+  # Names the general administration branch 'Warehouse' for inventory item instances
+  def branch_name_of( branch )
+    branch.name == 'General Administration' ? 'Warehouse' : branch.name
+  end
 end
